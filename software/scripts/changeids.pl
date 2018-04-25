8975716f74ddb45bafaf3659ed1b387dd0907872
FilAlves <38048444+FilAlves@users.noreply.github.com>
2018-04-25 20:39:31 +0100

    Add files via upload
    Software necessary for the run.

---
+++ software/scripts/changeids.pl
@@ -0,0 +1,27 @@
+#!/usr/local/bin/perl
+
+if (!$ARGV[0]){
+        print "introduce name of fasta file: ";
+        $name=<STDIN>;
+        chomp $name;
+}else{
+        $name=".temp/",$ARGV[0];
+        chomp $name;
+}
+($id,$ext)=split(/\./,$name);
+                open (codigos,"$name") || die "Error: problem opening fasta file\n";
+                open (results,">$id"."ids_out.fasta");
+
+
+                $count=0;
+                while (<codigos>){
+                        chomp $_;
+                        if ($_=~/^\>/){
+				($kk,$resto)=split(/\>/,$_);
+                                $count++;
+                                print results ">$count"."_$resto\n";
+                        }else{
+                                print results "$_\n";
+                                }
+                }
+
---
+++ software/scripts/extraeseqs.pl
@@ -0,0 +1,37 @@
+#!/usr/local/bin/perl
+####Programa para obtener la longitud de cada una de las secuencias de un fichero fasta;
+if ((!$ARGV[0]) && (!$ARGV[1])){
+	print "introduce name of fasta file: ";
+	$name=<STDIN>;
+	chomp $name;
+	print "introduce name of micros list (ID start end seq_length): ";
+	$list=<STDIN>;
+	chomp $list;
+}else{
+	$name=$ARGV[0];
+	chomp $name;
+	$list=$ARGV[1];
+	chomp $list;
+}
+		
+		open (fasta,"$name") || die "Error: problem opening fasta file\n";
+		open (results,">$lista"."noSSR.fasta");
+	
+	while (<fasta>){
+			chomp $_; 
+			if ($_=~/^\>/){
+			($simbol,$ID)=split(/\>/,$_);
+			}else{
+				$seq=$_;
+			$fasta{$ID}=$seq;
+			}
+		}
+	open (table, "$list") || die "Error: problem opening table\n";
+
+while (<table>){
+	chomp $_;
+	$identificador=$_;
+
+print results ">$identificador\n$fasta{$identificador}\n";
+
+}
