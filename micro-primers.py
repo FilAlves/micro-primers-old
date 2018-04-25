24d0883a84d83fb603cbc8a07a4cd2890ddf96ae
FilAlves <38048444+FilAlves@users.noreply.github.com>
2018-04-25 20:41:19 +0100

    Add files via upload
    Corrected Trimmomatic() and grep().
    Initial work on misa() and ids().

--- micro-primers.py
+++ micro-primers.py
@@ -8,11 +8,11 @@ lines = file.read().splitlines()
 #iniciation of trimmomatic
 def trimmomatic():
     os.system(
-        "java -jar software/trimmomatic-0.36/trimmomatic-0.36.jar PE -phred33 "
+        "java -jar software/Trimmomatic-0.36/trimmomatic-0.36.jar PE -phred33 "
         "%s %s "
-        "trim_out_trimmed_R1.fastq trim_out_unpaired_R1.fastq "
-        "trim_out_trimmed_R2.fastq trim_out_unpaired_R2.fastq "
-        "ILLUMINACLIP:/           FALTA RESTO DO PATH            /software/trimmomatic-0.33/adapters/TruSeq2-PE.fa:2:30:10 "
+        ".temp/trim_out_trimmed_R1.fastq .temp/trim_out_unpaired_R1.fastq "
+        ".temp/trim_out_trimmed_R2.fastq .temp/trim_out_unpaired_R2.fastq "
+        "ILLUMINACLIP:/home/filalves/projecto/software/Trimmomatic-0.36/adapters/TruSeq2-PE.fa:2:30:10 "
         "LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15"
         %(lines[0], lines[1])
         )
@@ -28,21 +28,30 @@ def flash():
     os.system("software/FLASH-1.2.11/flash .temp/cut_out_nolink_R1.fastq .temp/cut_out_nolink_R2.fastq -M 220 -o .temp/flash_out 2>&1 |"
              " tee flash.log")
 
-#hkj
-#os.system("grep -B1 '^GATC\(\w*\)GATC$' flash_out.extendedFrags.fastq | sed 's/^@/>/' | perl -pe 's/--\n//g' > good_frags.fasta")
+def grep():
+    os.system("grep -B1 '^GATC\(\w*\)GATC$' .temp/flash_out.extendedFrags.fastq | sed 's/^@/>/' | perl -pe 's/--\n//g' > grep_out.fasta")
 
 #Change id's
-#os.system("perl changeids.pl good_frags.fasta")
+def ids():
+    os.system("perl software/scripts/changeids.pl grep_out.fasta")
 
 #Search Microssatelites
-#os.system("../software/misa.pl good_frags_idchanged.fasta")
+def misa():
+    os.system("perl misa.pl ids_out.fasta")
+
 
 #Length Calculation
-#os.system("perl ../micros_joint/extraelength.pl good_frags_idchanged.fasta")
+def length_calc():
+    os.system("perl ../micros_joint/extraelength.pl good_frags_idchanged.fasta")
 
 
-
+trimmomatic()
+cutadapt()
+flash()
+ids()
 
 
 #cutadapt()       A FUNCIONAR
 #flash()          A FUNCIONAR
+#ids()            Script não aceita o path na pasta .temp/
+#misa()            Mensagem de erro
